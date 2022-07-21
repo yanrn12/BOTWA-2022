7c37e49a0d91d9c2fd7cd750d3e7fb26bb3db2a8
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-07-21 21:27:25 +0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -1,16 +1,1 @@
-/**
-* jangan ganti ya kakak kakak sekalian
-* ini cuma buat ninggalin credit gw doang :)
-* jangan dihilangin, boleh di tambahin 🐦
-**/
-
-let handler = async (m, { conn }) => {
-bear = "Source Code"
-let esce = 'Big Thanks to\n\n Allah swt\nMy friends'
-conn.sendButtonImg(m.chat, fla + 'Thanks To', esce, wm2, 'Menu', '.menu', m) 
-}
-handler.help = ['tqto', 'team']
-handler.tags = ['info']
-handler.command = /^(tqto|team)$/i
-
-module.exports = handler
+function _0x25ce(){const _0x40d8ab=['chat','Menu','280060HQECKI','20968ZBzOor','sendButtonImg','help','182AgCvve','107180QbtIgE','31448utpAAo','17402nBJaYA','team','48KsZUdU','2184duZvNE','Thanks\x20To','exports','90JGPtiH','1500XOjIJF','info','tags','Source\x20Code','tqto','Big\x20Thanks\x20to\x0a\x0a\x20Allah\x20swt\x0aMy\x20friends\x0aTeam\x20Recode\x0ahttps://github.com/BOTCAHX','349040HcIeNR','5995AvUueI'];_0x25ce=function(){return _0x40d8ab;};return _0x25ce();}const _0x1e8493=_0x952c;(function(_0x168092,_0x4a805f){const _0x4c9a4a=_0x952c,_0x521a5d=_0x168092();while(!![]){try{const _0x487127=-parseInt(_0x4c9a4a(0xeb))/0x1+-parseInt(_0x4c9a4a(0xef))/0x2+parseInt(_0x4c9a4a(0xf8))/0x3*(parseInt(_0x4c9a4a(0xf5))/0x4)+parseInt(_0x4c9a4a(0xec))/0x5*(parseInt(_0x4c9a4a(0xfd))/0x6)+-parseInt(_0x4c9a4a(0xf3))/0x7*(parseInt(_0x4c9a4a(0xf0))/0x8)+-parseInt(_0x4c9a4a(0xfc))/0x9*(-parseInt(_0x4c9a4a(0xf4))/0xa)+-parseInt(_0x4c9a4a(0xf6))/0xb*(-parseInt(_0x4c9a4a(0xf9))/0xc);if(_0x487127===_0x4a805f)break;else _0x521a5d['push'](_0x521a5d['shift']());}catch(_0x5e539c){_0x521a5d['push'](_0x521a5d['shift']());}}}(_0x25ce,0x40506));function _0x952c(_0x2d8c88,_0x1f34dc){const _0x25cedf=_0x25ce();return _0x952c=function(_0x952c3b,_0x3369cf){_0x952c3b=_0x952c3b-0xe6;let _0x39f269=_0x25cedf[_0x952c3b];return _0x39f269;},_0x952c(_0x2d8c88,_0x1f34dc);}let handler=async(_0x411a95,{conn:_0x5659f2})=>{const _0x628903=_0x952c;bear=_0x628903(0xe8);let _0x29a0ee=_0x628903(0xea);_0x5659f2[_0x628903(0xf1)](_0x411a95[_0x628903(0xed)],fla+_0x628903(0xfa),_0x29a0ee,wm2,_0x628903(0xee),'.menu',_0x411a95);};handler[_0x1e8493(0xf2)]=[_0x1e8493(0xe9),_0x1e8493(0xf7)],handler[_0x1e8493(0xe7)]=[_0x1e8493(0xe6)],handler['command']=/^(tqto|team)$/i,module[_0x1e8493(0xfb)]=handler;
